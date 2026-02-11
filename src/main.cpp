57c31434e698868b53c2a60e6da37a3252bc9fc6
ansh-codr <anshloverao@gmail.com>
2026-02-11 13:53:18 +0530

    Define screen dimensions for OLED display in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#define Screen_Width 128
+#define Screen_Height 64
 void setup() {
 // write your initialization code here
 }
